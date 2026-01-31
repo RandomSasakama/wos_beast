e77d30d566ad8f7200ca540b42ae7b4df98ff8f1
sasakama <64120368+RandomSasakama@users.noreply.github.com>
2026-01-31 15:53:58 +0900

    app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from datetime import timedelta
 st.set_page_config(page_title="討伐計算ツール", layout="centered")
 st.title("討伐計算ツール")
 #st.sub
-st.text("野獣・巨獣の平均行軍時間、缶詰の数、使う行軍数を入力してください\n総ポイントと所要時間が出ます\n")
+st.text("野獣・巨獣の平均行軍時間、缶詰の数、使う行軍数を入力してください\n総ポイントと所要時間が出ます\nやっててよかったなかし式")
 
-b_travel = st.number_input("野獣行軍時間（秒） ", min_value=0, step=1)*2
+b_travel = st.number_input("\n野獣行軍時間（秒） ", min_value=0, step=1)*2
 r_travel = st.number_input("巨獣行軍時間（秒）", min_value=0, step=1)
 sta_num = st.number_input("缶詰の数 ", min_value=0, step=1)*10
 use_for_total = st.number_input("行軍数 ", min_value=0, step=1)
@@ -54,6 +54,7 @@ if st.button("計算する"):
 
     df = pd.DataFrame(val)
     st.dataframe(df, use_container_width=True, hide_index=True)
+
 
 
 
